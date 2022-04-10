84720abd140226f4ecbf4158db6fc780efa71fa4
trainsn <shineng9664@gmail.com>
2022-04-10 16:24:25 -0400

    first commit

---
+++ build/support/glfw/cmake_uninstall.cmake
@@ -0,0 +1,29 @@
+
+if (NOT EXISTS "/users/PAS0027/trainsn/opengl/particle_volume/build/support/glfw/install_manifest.txt")
+  message(FATAL_ERROR "Cannot find install manifest: \"/users/PAS0027/trainsn/opengl/particle_volume/build/support/glfw/install_manifest.txt\"")
+endif()
+
+file(READ "/users/PAS0027/trainsn/opengl/particle_volume/build/support/glfw/install_manifest.txt" files)
+string(REGEX REPLACE "\n" ";" files "${files}")
+
+foreach (file ${files})
+  message(STATUS "Uninstalling \"$ENV{DESTDIR}${file}\"")
+  if (EXISTS "$ENV{DESTDIR}${file}")
+    exec_program("/usr/local/cmake/3.17.2/bin/cmake" ARGS "-E remove \"$ENV{DESTDIR}${file}\""
+                 OUTPUT_VARIABLE rm_out
+                 RETURN_VALUE rm_retval)
+    if (NOT "${rm_retval}" STREQUAL 0)
+      MESSAGE(FATAL_ERROR "Problem when removing \"$ENV{DESTDIR}${file}\"")
+    endif()
+  elseif (IS_SYMLINK "$ENV{DESTDIR}${file}")
+    EXEC_PROGRAM("/usr/local/cmake/3.17.2/bin/cmake" ARGS "-E remove \"$ENV{DESTDIR}${file}\""
+                 OUTPUT_VARIABLE rm_out
+                 RETURN_VALUE rm_retval)
+    if (NOT "${rm_retval}" STREQUAL 0)
+      message(FATAL_ERROR "Problem when removing symlink \"$ENV{DESTDIR}${file}\"")
+    endif()
+  else()
+    message(STATUS "File \"$ENV{DESTDIR}${file}\" does not exist.")
+  endif()
+endforeach()
+
